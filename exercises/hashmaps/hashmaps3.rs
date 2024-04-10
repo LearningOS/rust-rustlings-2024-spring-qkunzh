fe0aa48aed70d672722ca04e718e7957c5c84420
qkunzh <shouleizhang@qq.com>
2024-04-10 09:17:56 +0800

    ci update

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,7 +14,6 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
@@ -46,12 +45,12 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
-        let mut team1_record = scores.entry(team_1_name).or_insert_with(Team::new);
-        team1_record.value().goals_scored += team_1_score;
-        team1_record.value().goals_conceded += team_2_score;
-        let mut team2_record = scores.entry(team_2_name).or_insert_with(Team::new);
-        team2_record.value().goals_scored += team_2_score;
-        team2_record.value().goals_conceded += team_1_score;
+        let team1_record = scores.entry(team_1_name).or_insert_with(Team::new);
+        team1_record.goals_scored += team_1_score;
+        team1_record.goals_conceded += team_2_score;
+        let  team2_record = scores.entry(team_2_name).or_insert_with(Team::new);
+        team2_record.goals_scored += team_2_score;
+        team2_record.goals_conceded += team_1_score;
     }
     scores
 }
